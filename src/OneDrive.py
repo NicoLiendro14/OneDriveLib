be3ec5d16e08cb6c1f03435905602cc6514bd06e
Nicolas <nicolasliendro0697@gmail.com>
2021-02-21 00:32:30 -0300

    Add docs to some methods.

--- src/OneDrive.py
+++ src/OneDrive.py
@@ -14,6 +14,23 @@ class OneDrive:
         self.path_credentials = path_credentials
 
     def get_token(self, auth_code, grant_type):
+        """ Get the access_token or refresh_token.
+
+        The token that is obtained depends if it is the first time that the user is authenticated or not.
+
+        Parameters
+        ----------
+        auth_code : dict
+            Contains code or refresh_token
+        grant_type : str
+            Type of grant_type, it could be code or refresh_token
+
+        Returns
+        -------
+        dict
+            a json with the credentials
+        """
+
         url, params = self.build_request(auth_code, grant_type)
         response = requests.post(url, data=params)
         json_response = json.loads(response.text)
@@ -23,6 +40,24 @@ class OneDrive:
         return json_response
 
     def build_request(self, auth_code, grant_type):
+        """ Build the request.
+
+        It depends if it is access_token or refresh_token.
+
+        Parameters
+        ----------
+        auth_code : dict
+            Contains code or refresh_token
+        grant_type : str
+            Type of grant_type, it could be code or refresh_token
+
+        Returns
+        -------
+        string, dict
+            a formed url and a dict with parameters
+
+        """
+
         params = {
             'client_id': self.client_id,
             'scope': self.scope,
@@ -35,18 +70,34 @@ class OneDrive:
         return url, params
 
     def create_tokens_file(self, credentials):
+        """ Create a json with credentials.
+
+        Create a json with credentials.
+
+        Parameters
+        ----------
+        credentials : dict
+            Contains the credentials
+
+        """
+
         with open(self.path_credentials, 'w') as outfile:
             json.dump(credentials, outfile)
 
     def get_items(self):
+        """ List children in the root of the current user's drive.
+
+        Create a json with credentials.
+
+        Returns
+        -------
+        dict
+            folders in root
+        """
         headers = {
             'Authorization': 'Bearer ' + self.access_token
         }
         url = "https://graph.microsoft.com/v1.0/me/drive/root/children"
         response = requests.get(url, headers=headers)
-        print(response.text)
-        return response
-
-
-
-
+        json_response = json.loads(response.text)
+        return json_response
